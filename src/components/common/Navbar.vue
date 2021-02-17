a67dc7d0b50874f5cdb55bc0c4518254305da3b5
Anton Khokhlov <vorkakashek@bk.ru>
2021-02-17 14:47:51 +0300

    1

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id='nav'>im navbar! :)</div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style scoped>
+
+#nav {
+    background-color: antiquewhite;
+    padding: 20px 0;
+}
+
+</style>
